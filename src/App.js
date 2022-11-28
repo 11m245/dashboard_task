88a17f409d14143484eb5a15596a10eaa61578ed
11m245 <sivaraj2siva@gmail.com>
2022-11-29 00:35:49 +0530

    update task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretUp, faCaretDown, faChartLine, faCoins, faDollar, faSignal } from '@fortawesome/free-solid-svg-icons'
+import LinearProgress from '@mui/material/LinearProgress';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
+    </div>
+  );
+}
+
+function Dashboard() {
+
+  return (
+    <div>
+      <h1>Welcome to Dashboard</h1>
+      <SummaryBoxList />
+    </div>
+  );
+}
+
+
+function SummaryBoxList() {
+  const dataList = [
+    {
+      number: 568,
+      percent: 0.7,
+      icon: faChartLine,
+      text: "Number Of Sales",
+      time: "Last Month",
+      iconColor: "rgb(135, 96, 251)",
+      performance: "up",
+      type: "count"
+    },
+    {
+      number: "12,897",
+      percent: 0.43,
+      icon: faCoins,
+      text: "New Revenue",
+      time: "Last Month",
+      iconColor: "#eb6f33",
+      performance: "down",
+      type: "money"
+    },
+    {
+      number: "11,234",
+      percent: 1.44,
+      icon: faDollar,
+      text: "Total Cost",
+      time: "Last Month",
+      iconColor: "#03c895",
+      performance: "down",
+      type: "money"
+    },
+    {
+      number: "789",
+      percent: 0.9,
+      icon: faSignal,
+      text: "Profit By Sale",
+      time: "Last Month",
+      iconColor: "#01b8ff",
+      performance: "up",
+      type: "money"
+    }
+  ];
+
+
+  return (
+    <div className="summary-box-list">
+      {dataList.map((dt) => <SummaryBox data={dt} />)}
+    </div>
+  );
+
+}
+
+function SummaryBox({ data }) {
+  return (
+    <div className='summary-box-container'>
+      <div className="summary-box-spec">
+        <p className="summary-box-text">{data.text}</p>
+        <FontAwesomeIcon style={{ color: data.iconColor }} icon={data.icon} />
+      </div>
+      <h2 className="summary-box-number">{data.type === "money" ? "$" : null}{data.number}</h2>
+      <LinearProgress color="secondary" variant="determinate" value={50} />
+      <div className="summary-box-time-container">
+        <p className="time">{data.time}</p>
+        <p className="percent">
+          <FontAwesomeIcon style={{ color: data.performance === "up" ? "green" : "red" }} icon={data.performance === "up" ? faCaretUp : faCaretDown} />
+          {" "}{data.percent}%</p>
+      </div>
     </div>
   );
 }
